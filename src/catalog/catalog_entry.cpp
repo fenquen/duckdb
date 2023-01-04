d5f478a9f20ead41face3bb568a7266e9b6bb74b
fenquen <fenquen@163.com>
2023-01-04 21:39:15 +0800

    writing style

--- src/catalog/catalog_entry.cpp
+++ src/catalog/catalog_entry.cpp
@@ -4,32 +4,32 @@
 
 namespace duckdb {
 
-CatalogEntry::CatalogEntry(CatalogType type, Catalog *catalog_p, string name_p)
-    : oid(catalog_p->ModifyCatalog()), type(type), catalog(catalog_p), set(nullptr), name(move(name_p)), deleted(false),
-      temporary(false), internal(false), parent(nullptr) {
-}
+    CatalogEntry::CatalogEntry(CatalogType type, Catalog *catalog_p, string name_p)
+            : oid(catalog_p->ModifyCatalog()), type(type), catalog(catalog_p), set(nullptr), name(move(name_p)), deleted(false),
+              temporary(false), internal(false), parent(nullptr) {
+    }
 
-CatalogEntry::~CatalogEntry() {
-}
+    CatalogEntry::~CatalogEntry() {
+    }
 
-void CatalogEntry::SetAsRoot() {
-}
+    void CatalogEntry::SetAsRoot() {
+    }
 
 // LCOV_EXCL_START
-unique_ptr<CatalogEntry> CatalogEntry::AlterEntry(ClientContext &context, AlterInfo *info) {
-	throw InternalException("Unsupported alter type for catalog entry!");
-}
+    unique_ptr<CatalogEntry> CatalogEntry::AlterEntry(ClientContext &context, AlterInfo *info) {
+        throw InternalException("Unsupported alter type for catalog entry!");
+    }
 
-void CatalogEntry::UndoAlter(ClientContext &context, AlterInfo *info) {
-}
+    void CatalogEntry::UndoAlter(ClientContext &context, AlterInfo *info) {
+    }
 
-unique_ptr<CatalogEntry> CatalogEntry::Copy(ClientContext &context) {
-	throw InternalException("Unsupported copy type for catalog entry!");
-}
+    unique_ptr<CatalogEntry> CatalogEntry::Copy(ClientContext &context) {
+        throw InternalException("Unsupported copy type for catalog entry!");
+    }
 
-string CatalogEntry::ToSQL() {
-	throw InternalException("Unsupported catalog type for ToSQL()");
-}
+    string CatalogEntry::ToSQL() {
+        throw InternalException("Unsupported catalog type for ToSQL()");
+    }
 // LCOV_EXCL_STOP
 
 } // namespace duckdb
